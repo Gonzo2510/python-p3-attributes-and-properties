3195dcec8c7b33c401c1332a1e83a27bb6bb0522
Jacob Gonzales <jagonz@live.com>
2023-10-31 09:30:44 -0500

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -9,7 +9,20 @@ APPROVED_BREEDS = [
     "French Bulldog",
     "Pug",
     "Pointer"
-]
+] 
 
 class Dog:
-    pass
+    def __init__(self, name = 'Mutt', breed="Pointer"):
+
+        if type(name) != str:
+            print("Name must be string between 1 and 25 characters.")
+        elif 1 < len(name) < 25:
+            self.name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+        
+        if breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self.breed = breed
+
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,17 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name='no name', job='Sales'):
+        if type(name) != str:
+            print("Name must be string between 1 and 25 characters.")
+        elif 1 < len(name) < 25:
+            self.name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+        if job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self.job = job
+
+        
